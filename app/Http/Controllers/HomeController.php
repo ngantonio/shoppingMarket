8d88b8f2a3044835bcb3af91a62fcfef9fd22ab6
Gabriel Antonio <oliveiragabr@outlook.com>
2018-12-18 18:19:08 -0400

    Proyecto base Laravel

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //Llama al middleware de autenticacion
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('home');
+    }
+}
