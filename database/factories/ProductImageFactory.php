abf9494ddb8282688e43757eabdcb9c6fbf5c3ba
Gabriel Antonio <oliveiragabr@outlook.com>
2019-02-24 18:12:17 -0400

    Configuraciones necesarias para hacer deploy en heroku

--- database/factories/ProductImageFactory.php
+++ database/factories/ProductImageFactory.php
@@ -8,16 +8,5 @@ $factory->define(ProductImage::class, function (Faker $faker) {
 
         'url' => $faker->imageUrl(250,250),
         'product_id' => $faker->numberBetween(1,100)
-        /*
-                    $table->string('url');
-            # Imagen destacada
-            $table->boolean('featured')->default(false);
-
-            # Clave foranea para que se pueda identificar a que producto
-            #pertenece esa imagen
-            $table->integer('product_id')->unsigned();
-            $table->foreign('product_id')->references('id')->on('products')
-            $table->timestamps(); 
-        */
     ];
 });
